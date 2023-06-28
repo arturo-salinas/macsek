c90a82e05adf6f1eb32ac405d8ef70f758d2e5b0
Arturo Salinas <arturo.salinas@ericsson.com>
2023-06-28 23:23:10 +0200

    Fix errors in deployment
    Web site is totally functional.

--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -18,21 +18,21 @@ export default function MySwiper() {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide><img width="1200" src="/src/photos/1.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/2.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/3.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/4.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/9.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img width="1200" src="/src/photos/11.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/14.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/17.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/18.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/19.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img width="1200" src="/src/photos/21.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/22.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/23.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/25.jpeg"></img></SwiperSlide>
-      <SwiperSlide><img height="700" src="/src/photos/26.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/1.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/2.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/3.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/4.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/9.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/11.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/14.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/17.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/18.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/19.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/21.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/22.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/23.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/25.jpeg"></img></SwiperSlide>
+      <SwiperSlide><img className="test" src="/photos/26.jpeg"></img></SwiperSlide>
     </Swiper>
   );
 }
---
+++ src/components/about_menu.jsx
@@ -0,0 +1,10 @@
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/slide.css';
+
+export default function Example() {
+  return (
+    <Menu menuButton={<MenuButton>About</MenuButton>} transition>
+    </Menu>
+  );
+}
